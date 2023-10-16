ba6f9597eac3cc1065baadca8af5af70b95c5835
muxlisaumarova <148072058+muxlisaumarova@users.noreply.github.com>
2023-10-16 17:48:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,142 @@
+ # print("hello world")
+# print('hello world')
+# print('Men "lenovo" markasidagi noutbook sotib oldim')
+# print('djvh\nvjdf\n
+#  dvj')
+# print(2**10)
+# print(19//7)
+# print(25**(1/2))
+# print('ikkining kvadrati', 2**2, 'ga teng')
+# print('3x3=', 3*3)
+# ism="Muxlisa"
+# yosh=19
+# print(ism)
+# print(yosh)
+#
+# ism="Muxlisa"
+# print(ism)
+# ism="Ruxsora"
+# print(ism)
+#
+#
+# print("goood 🙂 ")
+# print("happy birthday 🥳 ")
+#
+#
+# ism='Muxlisa'
+# print("Mening ismim " + ism)
+# ism='Muxlisa '
+# familiya="Umarova"
+# print(ism + familiya)
+#
+#
+#
+#
+# ism='Muxlisa'
+# familiya="Umarova"
+# ism_sharif = f"{ism} {familiya}"
+# print(ism_sharif)
+#
+#
+# ism='Muxlisa'
+# familiya="Umarova"
+# print(f"Salom, mening ismim {ism} {familiya}!")
+#
+#
+# print("Hello world")
+# print("Hello \tworld")
+# print("Hello \nworld")
+
+# ism_sharif = 'muxlisa umarova'
+# # print(ism_sharif.capitalize())
+#
+# meva = "       olma               "
+# print("Men " + meva.lstrip() + " yaxshi ko'raman ")
+# print("Men " + meva.rstrip() + " yaxshi ko'raman ")
+# print("Men " + meva.strip() + " yaxshi ko'raman ")
+# print("Men " + meva + " yaxshi ko'
+
+# ism = input("Ismingiz nima?")
+# print("assalomu aleykum, " ism)
+# a = -20
+# b = 40
+# c = b/a
+# # print(c)
+#
+# aholi_soni = 7_436_355_123_234
+# print("Yer yuzining ", aholi_soni, "jami aholisi bor")
+
+# x, y, z =  10,  -7.25,  -30
+# print(x,  y,  z)
+#
+# ism = "Muxlisa"
+# yosh = 19
+# xabar = ism + ' ' + str(yosh) + ' yoshda'
+# print(xabar)
+
+# ism = "Muxlisa"
+# yosh = 19
+# print(type(ism))
+# print(type(yosh))
+#
+# # foydalanuvchini tug'ilgan yilini so'raymiz'
+# t_yil = int(input("Tug'ilgan yilingizni kiriting: "))
+# # foydalanuvchini yoshini hisoblaymiz
+# yosh = 2023 - t_yil
+# print("Siz " + str(yosh) + " yoshda ekansiz")
+
+# mevalar = ['olma','anjir','shaftoli',"o'rik"]
+# print("Birinchi meva:",mevalar[0])
+# print("Ikkinchi meva:",mevalar[1])
+# print("Uchinchi meva:",mevalar[2])
+# print("To'rtinchi meva:",mevalar[3])
+
+# mevalar = ['olma','anjir','shaftoli',"o'rik"]
+# print(mevalar)
+# print("Birinchi meva:",mevalar[0].title())
+# print("Ikkinchi meva:",mevalar[1].upper())
+
+# narhlar = [12000,18000,10900,22000]
+# print(narhlar[2]+narhlar[3])
+# narhlar = [12000,18000,10900,22000]
+# narhlar[0]=13000
+# narhlar[2]=11000
+# narhlar[3]=narhlar[3]+2000
+# print(narhlar)
+
+#
+# mevalar = ["olma","anjir","shaftoli","o'rik"]
+# mevalar.append("tarvuz")
+# print(mevalar)
+
+#
+# cars = []
+# cars.append('Lasetti')
+# cars.append('Nexia 3')
+# cars.append('Cobalt')
+# print(cars)
+
+# cars = ['Lasetti','Nexia 3','Cobalt']
+# cars.insert(0,'Malibu')
+# # print(cars)
+#
+#
+# mevalar = ['olma','anjir','shaftoli',"o'rik",'anor']
+# del mevalar[1]
+# print(mevalar)
+#
+#
+# mevalar = ['olma','anjir','shaftoli',"o'rik",'anor']
+# mevalar.remove("shaftoli")
+# print(mevalar)
+#
+# hayvonlar = ['it', 'mushuk','sigir',"qo'y", 'quyon','mushuk','sigir']
+# hayvonlar.remove('mushuk')
+# hayvonlar.remove('sigir')
+# print(hayvonlar)
+
+bozorlik = ["yog'",'un','piyoz','banan',"go'sht"]
+mahsulot = bozorlik.pop(3)
+print(f"Men '{mahsulot}' sotib oldim.")
+print("Olinmagan mahsulotlar: ", bozorlik )
+
